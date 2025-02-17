4c8e455cacb75eb01d8c04f9403393282fee6de8
joedono <fjzusi@gmail.com>
2025-02-17 13:54:08 -0500

    Started work on theme.Added banner. Removed search

--- template/header.php
+++ template/header.php
@@ -3,21 +3,16 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
         <?php wp_body_open(); ?>
-        <div id="wrapper" class="hfeed">
+        <div id="wrapper" class="hfeed container">
             <header id="header" role="banner">
                 <div id="branding">
-                    <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-                        <?php
-                        if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-                        echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-                        if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-                        ?>
-                    </div>
-                    <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/banner.jpg" alt="Lyndsey Luther" class="img-fluid" />
                 </div>
                 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                     <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
